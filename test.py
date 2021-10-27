9049aca989c9828634d1febe9a8f230e230a5176
Jacob <Jacobhwtn@gmail.com>
2021-10-27 11:49:09 -0500

    Changed how probabilities are combined.

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import normalize
 import random
 import numpy as np
 
-#test
-def main():
 
-    #A list of all the possible prorgessions
-    progs = ["I", "Im", "I7", "II", "IIm", "II7", "III", "IIIm", "III7", "IV", "IVm", "IV7", "V", "Vm", "V7",  "VI", "VIm", "VI7", "VII", "VIIm", "VII7"]
+# test
+def main():
+    # A list of all the possible prorgessions
+    progs = ["I", "Im", "I7", "II", "IIm", "II7", "III", "IIIm", "III7", "IV", "IVm", "IV7", "V", "Vm", "V7", "VI",
+             "VIm", "VI7", "VII", "VIIm", "VII7"]
 
     alg0Out = normalize.main(algorithm1.possibleProgressions(progs))
     alg1Out = []
@@ -20,7 +21,6 @@ def main():
         for part2 in progs:
             alg2Out.append(normalize.main(algorithm1.possibleProgressionsLast2(progs, part1, part2)))
 
-
     #Print out the probabilities for testing
     # print(alg0Out)
     #
@@ -32,11 +32,10 @@ def main():
     #     if sum(alg2Out[x]) > 0:
     #         print("Possibilities after " + str(progs[int(x / len(progs))]) + "  " + str(progs[x % len(progs)]) + "  " + str(alg2Out[x]))
 
-
     # Weigths for each section
-    prob0Weight = 1/3
-    prob1Weight = 1/3
-    prob2Weight = 1/3
+    prob0Weight = 1
+    prob1Weight = 2
+    prob2Weight = 4
 
     # Get the user inputed list of progressons seperated by commaas
     progsIn = input("Comma separated string of progressions: ")
@@ -44,34 +43,49 @@ def main():
     # remove spaces and turn comma seperated string into list
     progsIn = progsIn.replace(" ", "").split(",")
 
+    # If the user gives more than 2 elements remove all but the last two
+    if (len(progsIn)) > 2:
+        progsIn = progsIn[-2:]
+
     # If the user doesn't input a progression
     if progsIn[0] == '':
-        print(random.choices(progs, weights = alg0Out, k = 1))
+        print(random.choices(progs, weights=alg0Out, k=1))
 
     # If the user only inputs 1 progression
-    elif len(progsIn) == 1:
+    elif len(progsIn) == 1 and sum(alg1Out[progs.index(progsIn[0])]) != 0:
         weights0 = np.array(alg0Out) * prob0Weight
         weights1 = np.array(alg1Out[progs.index(progsIn[0])]) * prob1Weight
-        print(random.choices(progs, weights = weights0 + weights1, k = 1))
+
+        for x in range(len((weights0 * weights1))):
+            if (weights0 * weights1)[x] != 0:
+                print(str(progs[x]) + " " + str(normalize.main((weights0 * weights1))[x]))
+
+        print(random.choices(progs, weights=weights0 + weights1, k=1))
 
     # If the user inputs 2 progressions
-    elif len(progsIn) == 2:
+    elif len(progsIn) == 2 and sum(alg2Out[progs.index(progsIn[0]) * len(progs) + progs.index(progsIn[1])]) != 0:
         weights0 = np.array(alg0Out) * prob0Weight
         weights1 = np.array(alg1Out[progs.index(progsIn[0])]) * prob1Weight
         weights2 = np.array(alg2Out[progs.index(progsIn[0]) * len(progs) + progs.index(progsIn[1])]) * prob2Weight
 
-        # Print probabilities for testing
-        # for x in range(len((weights0 * weights1 * weights2))):
-        #     if (weights0 * weights1 * weights2)[x] != 0:
-        #         print(str(progs[x]) + " " + str((weights0 * weights1 * weights2)[x]))
+        newWeights = probability3(weights0, weights1, weights2)
 
-        print(random.choices(progs, weights=weights0 * weights1 * weights2, k=1))
+        # Print probabilities for testing
+        for x in range(len(newWeights)):
+            if normalize.main(newWeights)[x] != 0:
+                print(str(progs[x]) + " " + str(normalize.main(newWeights)[x]))
+
+        print(random.choices(progs, weights=newWeights, k=1))
 
     else:
         print("We don't have data for that progression")
 
     return None
 
+def probability3(P1, P2, P3):
+
+    return P1+P2+P3-(P1*P2)-(P1*P3)-(P2*P3)+(P1*P2*P3)
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
