46883e166fcb6b9f86a3dda831291fd561ddf1fb
losmon423 <90355485+losmon423@users.noreply.github.com>
2021-10-27 11:20:19 -0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -7,7 +7,9 @@ def main():
         info = []
         for row in reader:
             for i in range(len(row)):
-                if '\r\n' in row[i]:
+                if ' ' in row[i]:
+                    row[i] = ''
+                elif '\r\n' in row[i]:
                     row[i] = row[i][:-2]
                 elif row[i] == '' and row[i + 1] == '':
                     row = row[:i]
