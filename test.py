f90af3a7c62d373e0dd8982613c839345947227a
Jacob Howton <jacob@howton.com>
2021-10-27 09:00:01 -0500

    Changed the algorithms in algorithm1.py to return a list of probabilities.
    Fixed 2 errors in Digital Representation.csv where there were extra spacecs
    
    Made a function called normalize.py to make weights add up to 1
    
    Made test.py input a string of progressions from the user and then output a possible next chord depending on how many progressions were input.

--- test.py
+++ test.py
@@ -1,27 +1,74 @@
 import algorithm1
+import normalize
+import random
+import numpy as np
 
 #test
 def main():
+
+    #A list of all the possible prorgessions
     progs = ["I", "Im", "I7", "II", "IIm", "II7", "III", "IIIm", "III7", "IV", "IVm", "IV7", "V", "Vm", "V7",  "VI", "VIm", "VI7", "VII", "VIIm", "VII7"]
 
+    alg0Out = normalize.main(algorithm1.possibleProgressions(progs))
     alg1Out = []
     alg2Out = []
 
     for part in progs:
-        alg1Out.append(algorithm1.possibleProgressionsLast1(part))
+        alg1Out.append(normalize.main(algorithm1.possibleProgressionsLast1(progs, part)))
 
     for part1 in progs:
         for part2 in progs:
-            alg2Out.append(algorithm1.possibleProgressionsLast2(part1, part2))
+            alg2Out.append(normalize.main(algorithm1.possibleProgressionsLast2(progs, part1, part2)))
 
-    for x in range(len(alg1Out)):
-        if len(alg1Out[x]) > 0:
-            print("Possibilities for " + str(progs[x]) + "  " + str(alg1Out[x]))
 
-    for x in range(len(alg2Out)):
-        if len(alg2Out[x]) > 0:
-            print("Possibilities for " + str(progs[int(x / len(progs))]) + "  " + str(progs[x % len(progs)]) + "  " + str(alg2Out[x]))
+    #Print out the probabilities for testing
+    # print(alg0Out)
+    #
+    # for x in range(len(alg1Out)):
+    #     if sum(alg1Out[x]) > 0:
+    #         print("Possibilities after " + str(progs[x]) + "  " + str(alg1Out[x]))
+    #
+    # for x in range(len(alg2Out)):
+    #     if sum(alg2Out[x]) > 0:
+    #         print("Possibilities after " + str(progs[int(x / len(progs))]) + "  " + str(progs[x % len(progs)]) + "  " + str(alg2Out[x]))
 
+
+    # Weigths for each section
+    prob0Weight = 1/3
+    prob1Weight = 1/3
+    prob2Weight = 1/3
+
+    # Get the user inputed list of progressons seperated by commaas
+    progsIn = input("Comma separated string of progressions: ")
+
+    # remove spaces and turn comma seperated string into list
+    progsIn = progsIn.replace(" ", "").split(",")
+
+    # If the user doesn't input a progression
+    if progsIn[0] == '':
+        print(random.choices(progs, weights = alg0Out, k = 1))
+
+    # If the user only inputs 1 progression
+    elif len(progsIn) == 1:
+        weights0 = np.array(alg0Out) * prob0Weight
+        weights1 = np.array(alg1Out[progs.index(progsIn[0])]) * prob1Weight
+        print(random.choices(progs, weights = weights0 + weights1, k = 1))
+
+    # If the user inputs 2 progressions
+    elif len(progsIn) == 2:
+        weights0 = np.array(alg0Out) * prob0Weight
+        weights1 = np.array(alg1Out[progs.index(progsIn[0])]) * prob1Weight
+        weights2 = np.array(alg2Out[progs.index(progsIn[0]) * len(progs) + progs.index(progsIn[1])]) * prob2Weight
+
+        # Print probabilities for testing
+        # for x in range(len((weights0 * weights1 * weights2))):
+        #     if (weights0 * weights1 * weights2)[x] != 0:
+        #         print(str(progs[x]) + " " + str((weights0 * weights1 * weights2)[x]))
+
+        print(random.choices(progs, weights=weights0 * weights1 * weights2, k=1))
+
+    else:
+        print("We don't have data for that progression")
 
     return None
 
