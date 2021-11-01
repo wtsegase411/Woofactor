370f894f29cd6cee6497a5226e09d43a19845424
Jacob Howton <jacob@howton.com>
2021-11-01 08:59:24 -0500

    Removed the weights from the prediction algorithm and set it to prioritize the longest previous sequences.

--- predictor.py
+++ predictor.py
@@ -32,11 +32,6 @@ def predictior(progsIn):
     #     if sum(alg2Out[x]) > 0:
     #         print("Possibilities after " + str(progs[int(x / len(progs))]) + "  " + str(progs[x % len(progs)]) + "  " + str(alg2Out[x]))
 
-    # Weigths for each section
-    prob0Weight = 1
-    prob1Weight = 2
-    prob2Weight = 4
-
     # If the user gives more than 2 elements remove all but the last two
     if (len(progsIn)) > 2:
         workingProgs = progsIn[-2:]
@@ -46,30 +41,26 @@ def predictior(progsIn):
 
     # If the user inputs 2 progressions we can see in the data
     if len(workingProgs) == 2 and sum(alg2Out[progs.index(workingProgs[0]) * len(progs) + progs.index(workingProgs[1])]) != 0:
-        weights0 = np.array(alg0Out) * prob0Weight
-        weights1 = np.array(alg1Out[progs.index(workingProgs[0])]) * prob1Weight
-        weights2 = np.array(
-            alg2Out[progs.index(workingProgs[0]) * len(progs) + progs.index(workingProgs[1])]) * prob2Weight
 
-        newWeights = probability3(weights0, weights1, weights2)
+        weights2 = np.array(alg2Out[progs.index(workingProgs[0]) * len(progs) + progs.index(workingProgs[1])])
 
         # Print probabilities for testing
         # for x in range(len(newWeights)):
         #     if normalize.main(newWeights)[x] != 0:
         #         print(str(progs[x]) + " " + str(normalize.main(newWeights)[x]))
 
-        progsIn.append(random.choices(progs, weights=newWeights, k=1)[0])
+        progsIn.append(random.choices(progs, weights=weights2, k=1)[0])
 
     # If the user only inputs 1 progression
     elif len(progsIn) >= 1 and sum(alg1Out[progs.index(progsIn[0])]) != 0:
-        weights0 = np.array(alg0Out) * prob0Weight
-        weights1 = np.array(alg1Out[progs.index(progsIn[0])]) * prob1Weight
+
+        weights1 = np.array(alg1Out[progs.index(progsIn[0])])
 
         # for x in range(len((weights0 * weights1))):
         #     if (weights0 * weights1)[x] != 0:
         #         print(str(progs[x]) + " " + str(normalize.main((weights0 * weights1))[x]))
 
-        progsIn.append(random.choices(progs, weights=weights0 + weights1, k=1)[0])
+        progsIn.append(random.choices(progs, weights=weights1, k=1)[0])
 
     # If the user doesn't input a progression
     else:
